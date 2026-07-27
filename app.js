0661ce9022b80f76db0894330a5ea814222d2d1e
itsneeleshsingh <neeleshsinghdpsjkp.2483ns@gmail.com>
2026-07-28 00:09:18 +0530

    Basic functionality working

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 let boxes = document.querySelectorAll(".box");
 let turnX=true;
 
+const winPattern = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+];
+
 boxes.forEach((box) => {
     box.addEventListener("click",() => {
         if(turnX){
@@ -11,5 +22,21 @@ boxes.forEach((box) => {
             turnX=true;
         }
         box.disabled = true;
+
+        checkWinner();
     })
 })
+
+const checkWinner = () => {
+    for(let pattern of winPattern){
+        let posVal1 = boxes[pattern[0]].innerHTML;
+        let posVal2 = boxes[pattern[1]].innerHTML;
+        let posVal3 = boxes[pattern[2]].innerHTML;
+
+        if(posVal1!="" && posVal2!="" && posVal3!=""){
+            if(posVal1==posVal2 && posVal2==posVal3){
+                console.log("winner",posVal1);
+            }
+        }
+    }
+}   
